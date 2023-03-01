4eca4b9b2839dabb6c71cab35d4044e238e06185
Matheus Manzoli Pires <manzoli.contato@gmail.com>
2023-03-01 16:59:55 -0300

    feat: add voice 2 text

---
+++ app/components/_app/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const AppContainer = styled.div`
+    font-family: 'Inter', sans-serif;
+    overflow: hidden;
+`
+
+const App = ({children}) => {
+    return (
+        <AppContainer>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+                rel="stylesheet"
+            />
+            <div>{children}</div>
+        </AppContainer>
+    )
+}
+
+export default App
---
+++ app/pages/home.tsx
@@ -0,0 +1,135 @@
+import React, {useState, useEffect, useRef} from 'react'
+import {
+    Container,
+    SearchInput,
+    VoiceButton,
+    ListeningContainer,
+    PulseText,
+    Badge,
+    Iframe,
+    ProductList,
+    ProductTile
+} from './homeStyle'
+
+export default function Home() {
+    const inputRef = useRef('')
+    const [firstRender, setFirstRender] = useState(true)
+    const [text, setText] = useState('')
+    const [isListening, setIsListening] = useState(false)
+    const [hasVoiceSupport, setHasVoiceSupport] = useState(false)
+    const [products, setProducts] = useState({
+        products: []
+    })
+
+    // Check if browser has voice support
+    useEffect(() => {
+        if (window.SpeechRecognition || window.webkitSpeechRecognition) {
+            setHasVoiceSupport(true)
+        }
+    }, [])
+
+    const searchInProducts = () => {
+        setProducts({
+            products: []
+        })
+        const route = `https://dummyjson.com/products/search?q=${text}`
+
+        fetch(route)
+            .then((response) => response.json())
+            .then((data) => {
+                setProducts(data)
+            })
+    }
+
+    const GetSpeech = () => {
+        if (firstRender) setFirstRender(false)
+
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+        const recognition = new SpeechRecognition()
+        recognition.lang = 'pt-BR'
+
+        recognition.onstart = () => {
+            setIsListening(true)
+        }
+
+        recognition.onspeechend = () => {
+            recognition.stop()
+            setIsListening(false)
+        }
+
+        recognition.onresult = (result) => {
+            const transcript = result.results[0][0].transcript
+            setText(transcript)
+            inputRef.current.value = transcript
+        }
+
+        recognition.start()
+    }
+
+    const handleSearch = (e) => {
+        if (firstRender) setFirstRender(false)
+        const {value} = e.target
+
+        setText(value)
+    }
+
+    useEffect(() => {
+        searchInProducts()
+    }, [text])
+
+    return (
+        <div>
+            <Container>
+                <SearchInput
+                    type="search"
+                    placeholder="Pesquise Aqui..."
+                    onChange={handleSearch}
+                    onSearch={handleSearch}
+                    ref={inputRef}
+                />
+                {hasVoiceSupport && <VoiceButton onClick={GetSpeech}>🎤</VoiceButton>}
+            </Container>
+            {isListening && (
+                <ListeningContainer>
+                    <PulseText>{isListening ? '🎤 Ouvindo sua linda voz...' : null}</PulseText>
+                </ListeningContainer>
+            )}
+            {firstRender && (
+                <>
+                    <h1>
+                        {hasVoiceSupport ? 'Diga ou digite' : 'Digite'} para procurar por seu
+                        produto
+                    </h1>
+                    <Badge>Tente palavras como "Iphone", "HP", "Samsung"</Badge>
+                    <Iframe
+                        src="https://embed.lottiefiles.com/animation/103853"
+                        frameBorder="0"
+                    ></Iframe>
+                </>
+            )}
+            {text && (
+                <div>
+                    <h1>Pesquisando por {text}</h1>
+                    <Badge>Produtos encontrados ({products.products.length})</Badge>
+                </div>
+            )}
+            {text && products.products.length === 0 && <h3>Nenhum produto encontrado</h3>}
+            {!firstRender && products.products.length > 0 && <h1>Exibindo todos os produtos</h1>}
+            <ProductList>
+                {!firstRender &&
+                    products.products.length > 0 &&
+                    products.products.map((product) => {
+                        return (
+                            <ProductTile key={product.id}>
+                                <img src={product.thumbnail} alt={product.title} />
+                                <h1>{product.title}</h1>
+                                <h3>${product.price}</h3>
+                                <p>{product.description}</p>
+                            </ProductTile>
+                        )
+                    })}
+            </ProductList>
+        </div>
+    )
+}
